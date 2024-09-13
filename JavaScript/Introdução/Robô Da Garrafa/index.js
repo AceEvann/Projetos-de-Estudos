be3dbe4d924726f0145648746e7b0a47108da429
Ace <102817244+AceEvann@users.noreply.github.com>
2024-09-13 13:38:55 -0300

    Update index.js

--- JavaScript/Introdução/Robô Da Garrafa/index.js
+++ JavaScript/Introdução/Robô Da Garrafa/index.js
@@ -9,7 +9,7 @@ console.log("moverCima");
 console.log("moverCima");
 console.log("moverDireita");
 console.log("moverDireita");
-// Console com a resposta final
+// Console com o pedido
 console.log("Compre " + calculoGarrafas(dinheiro, custoPorGarrafa) + " garrafas de leite.");
 console.log("moverEsquerda");
 console.log("moverEsquerda");
@@ -20,24 +20,31 @@ console.log("moverBaixo");
 console.log("moverEsquerda");
 console.log("moverEsquerda");
 console.log("entrarCasa");
-}
 
     // Retorna a função calculoTroco
     return calculoTroco(dinheiro, custoPorGarrafa);
 }
 
-// Função 
+// Função para saber quantas garrafas serão compradas
 function calculoGarrafas(dinheiroInicial, custoPorGarrafa) {
 
+    // Faz a divisão do dinheiro inicial pelo custo das garrafas
     var numeroDeGarrafas = Math.floor(dinheiroInicial / custoPorGarrafa);
 
+    // Retorna a variável numeroDeGarrafas
     return numeroDeGarrafas;
 }
 
+
+// Função para calcular o troco
 function calculoTroco(dinheiroInicial, custoPorGarrafa) {
+
+    // Pega o resto da divisão do dinheiro inicial pelo custo das garrafas
     var troco = dinheiroInicial % custoPorGarrafa;
+
+    // Retorn  variável troco
     return troco;
 }
 
-
+// Console final
 console.log("Olá senhor, aqui está " + calculoGarrafas(dinheiro, custoPorGarrafa) + " suas garrafas e seus " + pegarGarrafa(5, 3) + "R$ de troco.");
