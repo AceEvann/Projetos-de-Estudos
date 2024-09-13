4d5352f0b4f619f605e70de513698e1bea264ce3
Ace <102817244+AceEvann@users.noreply.github.com>
2024-09-13 11:20:51 -0300

    Create index.js

---
+++ JavaScript/Introdução/Vida Em Semanas/index.js
@@ -0,0 +1,19 @@
+function vidaEmSemanas(idade) {   
+        
+        // Subtrai 90 pela idade
+        var anosRestando = 90 - idade;
+
+        // Conta os dias
+        var dias = anosRestando * 365;
+
+        // Conta as semanas
+        var semanas = anosRestando * 52;
+
+        // Conta os anos
+        var meses = AnosRestando * 12;
+        
+        // Console with the asked message
+        console.log("Você tem " + dias + " dias, " + semanas + " semanas, e " + meses + " meses sobrando.");
+    }
+    
+vidaEmSemanas(56)
