0db834474a1a84dd09903bab2b72ca2f8139e6a7
Ace <102817244+AceEvann@users.noreply.github.com>
2024-09-13 10:58:39 -0300

    Create index.js

---
+++ JavaScript/Introdução/Exercício De Variáveis Em JavaScript/index.js
@@ -0,0 +1,13 @@
+function test() {
+    var a = "3";
+    var b = "8";
+    
+/***********Não altere o código acima 👆*******/
+//Digite seu código nas linhas 7 - 9:
+    var c = a;
+    a = b;
+    b = c;
+/***********Não mude o código abaixo 👇*******/
+
+    console.log("a is " + a);
+    console.log("b is " + b);
