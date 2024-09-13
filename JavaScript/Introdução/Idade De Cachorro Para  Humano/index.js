6d1a5306d9909cf7da124010f73c51ff0983d5e5
Ace <102817244+AceEvann@users.noreply.github.com>
2024-09-13 10:33:45 -0300

    Create index.js

---
+++ JavaScript/Introdução/Idade De Cachorro Para  Humano/index.js
@@ -0,0 +1,7 @@
+var idadeCachorro = prompt("Insira o nome do seu cachorro para descobrir quantos anos ele teriase fosse humano: ");
+
+// Calculo da idade do cachorro para humano
+var idadeHumano = ((idadeCachorro - 2) * 4) + 21;
+
+// Resposde a idade para o usuário
+alert("Seu cachorro tem " + idadeHumano + "anos em idade humana!");
